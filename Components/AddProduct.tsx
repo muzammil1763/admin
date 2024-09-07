39ae856135f33083a78100a5d2c16adaf0f080eb
Muzammil Aziz <muzammilk1763@gmil.com>
2024-09-07 19:20:37 +0500

    mm

--- Components/AddProduct.tsx
+++ Components/AddProduct.tsx
@@ -224,7 +224,7 @@ const ProductForm: React.FC = () => {
       )}
       <form
         onSubmit={handleSubmit}
-        className="w-[60vw] mt-20  space-y-6 p-24 bg-black text-white rounded-lg shadow-md mx-auto"
+        className="w-[60vw] mt-20  space-y-6 p-24 bg-white text-white  shadow-md mx-auto"
       >
         <div>
           <label
